b2587c127d46916cf35328f278e6d51ad81c34c7
Lowe__ <947875760@qq.com>
2017-11-17 20:46:25 +0800

    fontpage(jx3.html)can be used.

--- WebRoot/js/jx3.js
+++ WebRoot/js/jx3.js
@@ -1,28 +1,52 @@
-function getDataByGname(gname){
-	$("#logininfo").text("获取信息中...");
-	$.ajax({
-    	type: "GET",
-        url: "/kfmonitor/test.action",
-             dataType: "json",
-             success: function(data){
-             		   $("#logininfo").text("");
-                       $.each(data,function(i,n){
-                       var ss;
-    				   if(n["state"]==0){
-    				   	ss="维护";
-    				   }else{
-    				   	ss="正常";
-    				   }         
-                       $("#logininfo").append(n["gzone"]+" "+n["zserver"]+" "+ss+"<br>");
-                       
-                       });
-                       console.log(typeof data);
-                      },
-             error: function(){
-             	$("#logininfo").text("请求超时");
-             },
-     });
-}
 $(document).ready(function(){
 	getDataByGname("22");
+	/*$(".zserver").each(function(i,n){
+		$(".state:eq("+i+")").text("2523");
+	});*/
 });
+function getDataByGname(gname){
+	// $("#logininfo").text("获取信息中...");
+	$.ajax({
+		type: "GET",
+		url: "/kfmonitor/test.action",
+		dataType: "json",
+		success: function(data){
+			// changeToTable(data);
+			setToTable(data);
+		},
+		error: function(){
+			// $("#logininfo").text("请求超时");
+		},
+	});
+}
+/*function changeToTable(jsondata){//早期用来测试接口的函数
+	$("#logininfo").text("");
+	$.each(jsondata,function(i,n){
+		var ss;
+		if(n["state"]==0){
+			ss="维护";
+		}else{
+			ss="正常";
+		}         
+		$("#logininfo").append(n["gzone"]+" "+n["zserver"]+" "+ss+"<br>");
+		
+	});
+}*/
+function setToTable(jsondata){
+	var ss;
+	$(".zserver").each(function(zi,zn){
+		$.each(jsondata,function(ji,jn){
+			if(jn["zserver"].indexOf($(zn).text())>=0){
+				if(jn["state"]==0){
+					ss="维护";
+					$(".state:eq("+zi+")").css("color","red");
+				}else{
+					ss="正常";
+					$(".state:eq("+zi+")").css("color","green");
+				}   
+				$(".state:eq("+zi+")").text(ss);
+			}
+			/*$(".state:eq("+i+")").text("2523");*/
+		});	
+	});
+}
